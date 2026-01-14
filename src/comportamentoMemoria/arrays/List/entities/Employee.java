cc1dc8a5af9b21dfb408feccd2b1cfc13e5c1cbe
Arthur Freitas <freitarthur176@gmail.com>
2026-01-14 10:32:48 -0300

    continue exercise

--- src/comportamentoMemoria/arrays/List/entities/Employee.java
+++ src/comportamentoMemoria/arrays/List/entities/Employee.java
@@ -1,4 +1,39 @@
 package comportamentoMemoria.arrays.List.entities;
 
 public class Employee {
+
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        if (isValid()) {
+            this.name = name;
+            this.salary = salary;
+        } else {
+            this.name = "DADO_INVÁLIDO";
+            this.salary = 0.0;
+        }
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private boolean isValid() {
+        return name != null && !name.isEmpty() && salary > 0;
+    }
 }
--- src/comportamentoMemoria/arrays/List/exercise/application/Main.java
+++ src/comportamentoMemoria/arrays/List/exercise/application/Main.java
@@ -1,7 +1,44 @@
 package comportamentoMemoria.arrays.List.exercise.application;
 
+import comportamentoMemoria.arrays.List.entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many employees will be registered? ");
+        int nEmployees = sc.nextInt();
+
+        if (nEmployees > 0) {
+
+            // Cria Lista local com List
+            List<Employee> employees = new ArrayList<>();
+
+            for (int i = 0; i < employees.size(); i++) {
+
+                System.out.println("EMPLOYEE #"+ i +":");
+                System.out.print("ID: ");
+                int id = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("NAME: ");
+                String name = sc.nextLine();
+                System.out.print("SALARY: ");
+                double salary = sc.nextDouble();
+
+                // Instância o objeto
+
+                employees.add(new Employee(id, name, salary));
+            }
+        }
+
+        sc.close();
     }
 }
