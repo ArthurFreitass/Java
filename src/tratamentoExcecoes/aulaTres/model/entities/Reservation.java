fcfdb0344cb0e3cf32bddd6d6db2a9d796ab92eb
Arthur Freitas <freitarthur176@gmail.com>
2026-03-04 16:14:16 -0300

    Create Reservation.java

---
+++ src/tratamentoExcecoes/aulaTres/model/entities/Reservation.java
@@ -0,0 +1,44 @@
+package tratamentoExcecoes.aulaTres.model.entities;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkin;
+    private LocalDate checkout;
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, LocalDate checkin, LocalDate checkout) {
+        if (roomNumber == null || checkin == null || checkout == null) {
+            throw new IllegalArgumentException("Error: Invalid data! (Cannot be null)");
+        }
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public LocalDate getCheckin() {
+        return checkin;
+    }
+
+    public LocalDate getCheckout() {
+        return checkout;
+    }
+
+    public int duration() {
+        long days = ChronoUnit.DAYS.between(checkin, checkout);
+        return Math.toIntExact(Math.abs(days)); // Retorna a diferença de dias
+    }
+
+    public void updateDates(LocalDate checkin, LocalDate checkout) {
+
+    }
+}
