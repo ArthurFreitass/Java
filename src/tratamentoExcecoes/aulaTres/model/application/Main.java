b8e8f79e09c6160027d8205decbb07f24bebf2c1
Arthur Freitas <freitarthur176@gmail.com>
2026-03-05 15:54:29 -0300

    Update Main.java

--- src/tratamentoExcecoes/aulaTres/model/application/Main.java
+++ src/tratamentoExcecoes/aulaTres/model/application/Main.java
@@ -1,8 +1,45 @@
-package tratamentoExcecoes.aulaTres.model.application;
+package tratamentoExcecoes.aulaTres.application;
+
+import tratamentoExcecoes.aulaTres.application.entities.Reservation;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Room number: ");
+            int roomNumber = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Check-in date (DD/MM/YYYY): ");
+            LocalDate checkin = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            System.out.print("Check-out date (DD/MM/YYYY): ");
+            LocalDate checkout = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+            System.out.println("\n"+reservation);
+
+            // Update
+            System.out.println("Update dates:");
+
+            System.out.print("Check-in date (DD/MM/YYYY): ");
+            checkin = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            System.out.print("Check-out date (DD/MM/YYYY): ");
+            checkout = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            reservation.updateDate(checkin, checkout);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            sc.close();
+        }
     }
 }
-
