48be4398bcc22398345e2777533afc17cd27e5a2
Arthur Freitas <freitarthur176@gmail.com>
2026-04-13 11:14:20 -0300

    Create FormatterHours.java

---
+++ src/interfaces/primeiraAula/model/Util/FormatterHours.java
@@ -0,0 +1,12 @@
+package interfaces.documentacao.primeiraAula.model.Util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FormatterHours {
+
+    public static LocalDateTime formatter (String timeStr) {
+       DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+       return LocalDateTime.parse(timeStr, fmt);
+    }
+}
