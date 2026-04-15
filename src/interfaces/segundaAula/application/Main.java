1d058310bceb00e6b222d70715edc651f3277cd0
Arthur Freitas <freitarthur176@gmail.com>
2026-04-15 15:57:00 -0300

    Update Main.java

--- src/interfaces/segundaAula/application/Main.java
+++ src/interfaces/segundaAula/application/Main.java
@@ -1,11 +1,10 @@
 package interfaces.segundaAula.primeiraRefatoracao.application;
 
-import interfaces.primeiraAula.model.Util.FormatterHours;
-import interfaces.segundaAula.primeiraRefatoracao.Util.Formatter;
+import interfaces.segundaAula.primeiraRefatoracao.model.util.Formatter;
 import interfaces.segundaAula.primeiraRefatoracao.model.entities.CarRental;
 import interfaces.segundaAula.primeiraRefatoracao.model.entities.Vehicle;
 import interfaces.segundaAula.primeiraRefatoracao.model.exceptions.DomainException;
-import interfaces.segundaAula.primeiraRefatoracao.service.RentalService;
+import interfaces.segundaAula.primeiraRefatoracao.model.service.RentalService;
 
 import java.time.LocalDateTime;
 import java.util.Locale;
@@ -14,22 +13,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+
         try (Scanner sc = new Scanner(System.in)) {
 
             System.out.println("\nARTHUR-LOCALIZA");
             System.out.println("================\n");
 
-            Locale.setDefault(Locale.US);
 
             System.out.println("Entre com os dados do aluguel");
             System.out.print("Modelo do carro: ");
             String carModel = sc.nextLine();
 
             System.out.print("\nRetirada (dd/MM/yyyy HH:mm): ");
-            LocalDateTime pickup = LocalDateTime.parse(sc.nextLine(), Formatter.fmt);
+            LocalDateTime pickup = LocalDateTime.parse(sc.nextLine(), Formatter.fmt());
 
             System.out.print("Retorno (dd/MM/yyyy HH:mm): ");
-            LocalDateTime deliveryCar = LocalDateTime.parse(sc.nextLine(), Formatter.fmt);
+            LocalDateTime deliveryCar = LocalDateTime.parse(sc.nextLine(), Formatter.fmt());
 
             CarRental carRental = new CarRental(pickup, deliveryCar, new Vehicle(carModel));
 
@@ -42,10 +42,11 @@ public class Main {
             System.out.print("Entre com o preço por dia: ");
             double priceDay = sc.nextDouble(); sc.nextLine();
 
-            RentalService rentalService = new RentalService(carRental); // Instância Invoice
-            rentalService.processInvoice(priceHour, priceDay);
+            RentalService rentalService = new RentalService(); // Instância
+            rentalService.processInvoice(carRental, priceHour, priceDay);
 
-            System.out.println(carRental.getInvoice());
+            System.out.println("Fatura:\n");
+            System.out.print(carRental.getInvoice());
 
         } catch (DomainException e) {
             System.out.println(e.getMessage());
