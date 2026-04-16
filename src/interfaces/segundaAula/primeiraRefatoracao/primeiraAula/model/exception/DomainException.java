13af56468b1ef6235f2122a22732961bdc64a86f
Arthur Freitas <freitarthur176@gmail.com>
2026-04-16 11:04:29 -0300

    Add files via upload

---
+++ src/interfaces/segundaAula/primeiraRefatoracao/primeiraAula/model/exception/DomainException.java
@@ -0,0 +1,7 @@
+package interfaces.primeiraAula.model.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
