71260e62f7536aa4d952385e164eae861225ca04
Arthur Freitas <freitarthur176@gmail.com>
2026-07-28 10:52:04 -0300

    Delete src/generics/set/exercise directory

--- src/generics/set/exercise/model/exceptions/DomainException.java
+++
@@ -1,7 +0,0 @@
-package generics.set.exercise.model.exceptions;
-
-public class DomainException extends RuntimeException {
-    public DomainException(String message) {
-        super(message);
-    }
-}
